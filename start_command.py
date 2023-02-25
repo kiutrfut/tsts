f1b1819effb164066e6dbe9dba07c0be6f6151a5
kiutrfut <99256842+kiutrfut@users.noreply.github.com>
2023-02-25 20:17:45 +0530

    Create start_command.py

---
+++ start_command.py
@@ -0,0 +1,16 @@
+import telegram
+
+# Create a bot instance using the bot token
+bot = telegram.Bot(token='your_bot_token')
+
+def start(update, context):
+    # Get the chat ID of the user who sent the message
+    chat_id = update.message.chat_id
+    # Send a message to the user
+    bot.send_message(chat_id=chat_id, text="Hello! Welcome to my bot.")
+
+# Create an instance of the telegram.ext.CommandHandler class for the /start command
+start_handler = telegram.ext.CommandHandler('start', start)
+
+# Add the command handler to the dispatcher
+dispatcher.add_handler(start_handler)
