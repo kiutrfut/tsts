a117e0e56b1fcf67c2681c8e9aa5cb7e0a8aac6b
kiutrfut <99256842+kiutrfut@users.noreply.github.com>
2023-02-25 20:19:11 +0530

    Update and rename start_command.py to start.py

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 import telegram
 
 # Create a bot instance using the bot token
-bot = telegram.Bot(token='your_bot_token')
+bot = telegram.Bot(token='5562112612:AAH7Sbz2iIAdoPknjv0FnuiNbiDa_5OFYQA')
 
 def start(update, context):
     # Get the chat ID of the user who sent the message
